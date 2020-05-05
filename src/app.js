fb248f452c73f63245156c6532d6fdba7dc9dcd6
veecee <valentine424@yahoo.com>
2020-05-05 06:18:48 +0100

    styled footer, added a viewport metatag, and changed server port

--- src/app.js
+++ src/app.js
@@ -68,8 +68,7 @@ app.get('*', (req, res) => {
 })
 
 
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
 })
